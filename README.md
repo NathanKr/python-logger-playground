2185a7577624f9fbfecc3ed06cd1eedcfa00fe8f
Nathan Krasney <natankrasney@gmail.com>
2021-06-13 13:02:16 +0300

    ...

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# python-logger-playground
+<h2>Motivation</h2>
+basic python logger
+
+
+<h2>common_logger</h2>
+Here the root logger is defined and module utils1 and utils2 use it to write to root.log
+
+<h2>logger_per_module</h2>
+<ul>
+<li>Here utils1.py and utils2.py have their own logger and they write to utils1.log and utils2.log repectively</li>
+<li>because the root logger is defined in app.py it will be used also by utils1.py and utils2.py</li>
+<li>utils1.py is using also console logger</li>
+</ul>
+
+
--- logger_per_module/log_utils.py
+++ logger_per_module/log_utils.py
@@ -2,6 +2,20 @@ import logging
 
 
 def get_module_logger(module_name : str, log_level : int,formatter : str)->logging.Logger:
+    """
+        get logger with file handler
+        the log file is the same as the module name with extension .log
+
+    Args:
+        module_name (str): pass here__name__
+        log_level (int): pass here e.g. logger.DEBUG
+        formatter (str): pass here e.g. 'logger name : %(name)s , %(levelname)s,
+                                        func : %(funcName)s , %(message)s , module : %(module)s ,
+                                        line :  %(lineno)d , %(asctime)s'  
+
+    Returns:
+        logging.Logger: [description]
+    """
     logger = logging.getLogger(module_name) # use with module_name
     logger.setLevel(log_level) # e.g logging.DEBUG
 
