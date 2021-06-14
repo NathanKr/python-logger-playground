3f0252c68fd81954e2daf90704a192f027553243
Nathan Krasney <natankrasney@gmail.com>
2021-06-14 13:13:51 +0300

    use preaty print

--- log_utils/utils.py
+++ log_utils/utils.py
@@ -1,12 +1,20 @@
+from pprint import pformat
+
 def foo(num1,num2):
-    smart_print(pair_sep='\n',n1=num1,n2=num2)
+    print(args_to_string(n1=num1,n2=num2))
     return num1+num2
 
+SEP = '\n'
 
-def smart_print(pair_sep,**kwargs):
-    for key,value in kwargs.items():
-        print (f'{key} {value}',end=pair_sep)
+def args_to_string(**kwargs)->str:
+        args_as_string=''
+        index=0
+        for key,value in kwargs.items():
+            formated_value = pformat(value)
+            args_as_string += f'arg{index} {key} : {formated_value}{SEP}'
+            index += 1
 
+        return args_as_string     
 
 
 foo(1,2)
